7fd5ac05c6d98e4016fbe5b0bb20b39e291624f5
SAY007-DEV <mail.to.sayooj.kp@gmail.com>
2026-01-02 17:25:35 +0530

    bug fixed

--- Code404/src/App.jsx
+++ Code404/src/App.jsx
@@ -2,13 +2,15 @@ import { useState } from 'react'
 
 import './App.css'
 import NotFound from './Components/404-page/NotFound'
+import Error404 from './Components/404-page-2/Error404'
 
 function App() {
   
 
   return (
     <>
-      <NotFound/>
+      {/* <NotFound/> */}
+      <Error404/>
     </>
   )
 }
